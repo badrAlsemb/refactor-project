467e72c236b9a160f334018dbd52dd78e85ac8d7
Badr Alsemb <baderbalsemi@gmail.com>
2022-02-03 16:15:36 +0300

    Move the method to Rental Class

--- src/main/java/bad/robot/refactoring/chapter1/Rental.java
+++ src/main/java/bad/robot/refactoring/chapter1/Rental.java
@@ -21,4 +21,26 @@ public class Rental {
         return daysRented;
     }
 
+    double getChargeFor() {
+        double amount = 0;
+        switch (this.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                amount += 2;
+                if (this.getDaysRented() > 2) {
+                    amount += (this.getDaysRented() - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                amount += this.getDaysRented() * 3;
+                break;
+            case Movie.CHILDREN:
+                amount += 1.5;
+                if (this.getDaysRented() > 3) {
+                    amount += (this.getDaysRented() - 3) * 1.5;
+                }
+                break;
+        }
+        return amount;
+    }
+
 }
